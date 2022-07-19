2fc0c7fcd8787f3820fe8466649bf5a23b7a7b6e
Andrew <maryshevgit@gmail.com>
2022-07-20 01:45:01 +0300

    React Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainLayout from './components/layouts/MainLayout';
-import Admin from './pages/Admin/Admin';
-import Auth from './pages/Auth/Auth';
-import Cart from './pages/Cart/Cart';
-import Food from './pages/Food/Food';
+import Loader from './components/Loader/Loader';
 import Home from './pages/Home/Home';
-import OneFoodPage from './pages/OneFoodPage/OneFoodPage';
-import Orders from './pages/Orders/Orders';
+
+const Cart = lazy(() => import(/*webpackChunkName: "Cart" */ './pages/Cart/Cart'))
+const Admin = lazy(() => import(/*webpackChunkName: "Admin" */ './pages/Admin/Admin'))
+const Auth = lazy(() => import(/*webpackChunkName: "Auth" */ './pages/Auth/Auth'))
+const Food = lazy(() => import(/*webpackChunkName: "Food" */ './pages/Food/Food'))
+const OneFoodPage = lazy(() => import(/*webpackChunkName: "OneFoodPage" */ './pages/OneFoodPage/OneFoodPage'))
+const Orders = lazy(() => import(/*webpackChunkName: "Orders" */ './pages/Orders/Orders'))
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route path='' element={<Home />} />
-        <Route path='cart/orders' element={<Orders />} />
-        <Route path='login' element={<Auth />} />
-        <Route path='registration' element={<Auth />} />
-        <Route path='admin' element={<Admin />} />
-        <Route path='food' element={<Food />} />
-        <Route path='food/:id' element={<OneFoodPage />} />
-        <Route path='cart' element={<Cart />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route path='' element={<Home />} />
+          <Route path='cart/orders' element={<Orders />} />
+          <Route path='login' element={<Auth />} />
+          <Route path='registration' element={<Auth />} />
+          <Route path='admin' element={<Admin />} />
+          <Route path='food' element={<Food />} />
+          <Route path='food/:id' element={<OneFoodPage />} />
+          <Route path='cart' element={<Cart />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
